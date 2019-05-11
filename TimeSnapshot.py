1ebfe1dc45d813547bba4c0faa9ceb4ca1f30aee
aayush goyal <aayushgoyal9507@gmail.com>
2019-05-10 21:04:08 -0600

    Getting all key,pair values

--- TimeSnapshot.py
+++ TimeSnapshot.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-import simplejson
 import json
 
 class Node:
@@ -14,17 +12,18 @@ class Node:
 # with open('tjson.json') as f:
 #     myjson = simplejson.load(f, object_hook=lambda d: namedtuple('foo', d.keys())(*d.values()))
 
+
 with open("tjson.json", "r") as read_file:
-    data = json.load(read_file)
+    json = json.load(read_file)
 
 
 def timeSnapshot(json):
     result = []
     if json == str:
-
-        n = [Node(json, None, json, None)]
-        result.append(n)
-        return result
+        print "inside base condition"
+        # n = [Node(json, None, json, None)]
+        # result.append(n)
+        return json
 
     elif type(json) == list:
         for i in json:
@@ -33,18 +32,50 @@ def timeSnapshot(json):
 
     else:
         for key, value in json.iteritems():
+
+            print "KEYYYYYY", key
+            print "VALUEEEE", value
             if "Item" in key:
                 name = key.replace('Item', '')
 
                 version = name+"Versions"
                 timestamp = value["timestamp"]
-                print version, timestamp
-                
-                print value[version]
+
+                for versions in value[name+"Versions"]:
+
+                    data = versions["data"]
+                    ts = versions["timestamp"]
+
+                    # print data
+                    # print ts
+                    s = timeSnapshot(data)
+                    # node = Node("interiorList", None, s, ts)
+                    # result.append(node)
+
+            else:
+                if type(value)==str:
+                    print value
+                    # node = Node("string", None, value, None)
+                    # result.append([node])
+
+                elif type(value)==list:
+                    for element in value:
+                        s = timeSnapshot(element)
+                        # node = Node("list", None, s, None)
+                        # result.append(node)
+
+                elif type(value)==dict:
+                    # for k, v in value.iteritems():
+
+                    s = timeSnapshot(value)
+                    # print s
+                    # for k, v in value.iteritems():
+                    #     s = timeSnapshot(k)
+                    #     node = Node("pair", k, s, None)
+                    #     result.append(node)
 
 
-
-timeSnapshot(data)
+timeSnapshot(json)
 
 
 # with open("tjson.json", "r") as read_file:
