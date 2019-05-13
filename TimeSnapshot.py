fb117e9f87592c99142c697de302ea69974777bb
aayush goyal <aayushgoyal9507@gmail.com>
2019-05-13 16:15:14 -0600

    New Json added

--- TimeSnapshot.py
+++ TimeSnapshot.py
@@ -13,7 +13,7 @@ class Node:
 #     myjson = simplejson.load(f, object_hook=lambda d: namedtuple('foo', d.keys())(*d.values()))
 
 
-with open("tjson.json", "r") as read_file:
+with open("tjson2.json", "r") as read_file:
     json = json.load(read_file)
 
 
@@ -28,13 +28,12 @@ def timeSnapshot(json):
     elif type(json) == list:
         for i in json:
             s = timeSnapshot(i)
-            result.append(Node("list", None, s, None))
+            print "list", s
+            # result.append(Node("list", None, s, None))
 
     else:
         for key, value in json.iteritems():
 
-            print "KEYYYYYY", key
-            print "VALUEEEE", value
             if "Item" in key:
                 name = key.replace('Item', '')
 
@@ -46,21 +45,23 @@ def timeSnapshot(json):
                     data = versions["data"]
                     ts = versions["timestamp"]
 
-                    # print data
-                    # print ts
+                    print ts
                     s = timeSnapshot(data)
                     # node = Node("interiorList", None, s, ts)
                     # result.append(node)
 
             else:
-                if type(value)==str:
-                    print value
-                    # node = Node("string", None, value, None)
-                    # result.append([node])
+
+                print "typesss", type(key), type(value)
+                if type(value)==unicode:
+                    print "unicode", value
+                    node = Node("string", None, value, None)
+                    result.append([node])
 
                 elif type(value)==list:
                     for element in value:
                         s = timeSnapshot(element)
+                        print "list", s
                         # node = Node("list", None, s, None)
                         # result.append(node)
 
@@ -68,14 +69,17 @@ def timeSnapshot(json):
                     # for k, v in value.iteritems():
 
                     s = timeSnapshot(value)
-                    # print s
+                    print "Dict", s
                     # for k, v in value.iteritems():
                     #     s = timeSnapshot(k)
                     #     node = Node("pair", k, s, None)
                     #     result.append(node)
 
+    return result
 
-timeSnapshot(json)
+res = timeSnapshot(json)
+print "resulttt",res
+
 
 
 # with open("tjson.json", "r") as read_file:
