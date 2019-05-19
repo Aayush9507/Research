474e6ce0a1a648f591c946d0a5265d7938dd92aa
aayush goyal <aayushgoyal9507@gmail.com>
2019-05-18 19:06:47 -0600

    Added Overlap function
    This function checks whether two year ranges overlap

--- TimeSnapshot2.py
+++ TimeSnapshot2.py
@@ -1,11 +1,8 @@
 import json
-
-name = ''
-newjson = {}
-result = []
 jsonArray = []
 
-with open("tjson2.json", "r") as read_file:
+
+with open("tjson.json", "r") as read_file:
     data = json.load(read_file)
 
 
@@ -22,66 +19,40 @@ def timeSnapshot(json):
         for key, value in json.iteritems():
 
             if "Item" in key:
+
                 # print key
                 name = key.replace('Item', '')
                 version = name+"Versions"
                 timestamp = value["timestamp"]
-
-                # newjson.update({"data": {"specimen": {}}})
-
 
                 for versions in value[version]:
 
                     data = versions["data"]
                     ts = versions["timestamp"]
                     s = timeSnapshot(data)
-                    # print s
             else:
 
                 if type(value) == unicode:
                     s = timeSnapshot(value)
-                    # print "unicode", s
 
                 elif type(value) == list:
 
                     s = timeSnapshot(value)
 
-                    # print "list", s
-
                     for i in range(0, len(s)):
 
-                        # print type(s[i]), s[i]
-
-                        # print sorted(s[i])
                         for k, v in s[i].iteritems():
 
                             if k == "timestamp":
-                                # print k, v
                                 newjson = {}
                                 newjson.update({"data": {"specimen": {}}})
                                 newjson.update({"timestamp": v})
-                                # newjson2.update({"timestamp": v})
 
-                                # print newjson
                                 jsonArray.append(newjson)
-
-                            # if type(v) == dict:
-                            #     for k2, v2 in v.iteritems():
-                            #
-                            #         print k2, v2
-
-
-
-                                    # newjson["data"]["specimen"].update({k2: v2})
-
-
-
-                                    # print "new", newjson
 
                 elif type(value) == dict:
                     for k, v in value.iteritems():
                         s = timeSnapshot(v)
-                        # print s
 
     return jsonArray
 
@@ -99,21 +70,25 @@ def preprocess_json_array(arr):
     startArr = []
     endArr = []
 
-    for i in range(0, len(res_list)):
-        start, end = str(res_list[i]['timestamp']).split("-")
+    for j in range(0, len(res_list)):
+        start, end = str(res_list[j]['timestamp']).split("-")
+
         startArr.append(start)
+
         endArr.append(end)
 
     newArr = []
+
     smallest_start = min(startArr)
     max_end = max(endArr)
 
-
     while int(smallest_start) <= int(max_end):
+
         smallest_end = min(endArr)
         endArr.remove(smallest_end)
         newArr.append(str(smallest_start) + '-' + smallest_end)
         smallest_start = int(smallest_end) + 1
+
 
     json_array = []
 
@@ -127,18 +102,24 @@ def preprocess_json_array(arr):
     return json_array
 
 
-output_json_arr =  preprocess_json_array(arr)
+output_json_arr = preprocess_json_array(arr)
 
 
 for o in range(0, len(output_json_arr)):
 
-    print output_json_arr[o]
+    json1 = output_json_arr[o]
+    break
 
 
-    # jsonData = timeSnapshot(data)
-#
-# with open('result2.json', 'w') as fp:
-#     json.dump(jsonData, fp)
+def checkOverlap(json1,json2):
+
+    startA, endA = json1['timestamp'].split('-')
+    startB, endB = json2['timestamp'].split('-')
+
+    if (startA==startB) or (startA>startB and startA<=endB):
+        return True
+    else:
+        return False
 
 
 def timeSnapshot2(json):
@@ -154,35 +135,27 @@ def timeSnapshot2(json):
         for key, value in json.iteritems():
 
             if "Item" in key:
-                # print key
                 name = key.replace('Item', '')
                 version = name+"Versions"
                 timestamp = value["timestamp"]
-
-                # newjson.update({"data": {"specimen": {}}})
-
 
                 for versions in value[version]:
 
                     data = versions["data"]
                     ts = versions["timestamp"]
                     s = timeSnapshot2(data)
-                    # print s
             else:
 
                 if type(value) == unicode:
                     s = timeSnapshot2(value)
-                    # print "unicode", s
 
                 elif type(value) == list:
 
                     s = timeSnapshot2(value)
 
-                    # print "list", s
-
                     for i in range(0, len(s)):
 
-                        if s[i]['timestamp']==json1['timestamp']:
+                        if s[i]['timestamp']==json1['timestamp'] or checkOverlap(json1, s[i])==True:
 
                             for k, v in s[i].iteritems():
 
@@ -191,17 +164,20 @@ def timeSnapshot2(json):
 
                                         json1["data"]["specimen"].update({k2: v2})
 
-                    print "jsonnn", json1
+
 
                 elif type(value) == dict:
                     for k, v in value.iteritems():
                         s = timeSnapshot2(v)
                         # print s
 
-    # return json1
+    return json1
 
 
-# timeSnapshot2(data)
+ss = timeSnapshot2(data)
+
+with open('result.json', 'w') as fp:
+    json.dump(ss, fp)
 
 
 
